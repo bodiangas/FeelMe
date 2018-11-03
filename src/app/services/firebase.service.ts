d461d3286059d53f31c77b3fcc6a410af39a4baf
traoreba <balle.traore@etu.univ-grenoble-alpes.fr>
2018-11-03 12:55:52 +0100

    Changement mode d'enregistrement enregistrement avec comme clé le nom de la liste

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -14,7 +14,48 @@ export interface MovieList {
   providedIn: 'root'
 })
 export class FirebaseService {
-  private moviesListList: MovieList[] = [];
+  private moviesListList: MovieList[] = [
+    {
+      name: 'Liste 1',
+      movies: [
+        {
+          budget: 3000000,
+          adult: false,
+          title: 'Jolie film'
+        }, {
+          budget: 4000000,
+          adult: false,
+          title: 'Mauvais film'
+        }, {
+          budget: 2000000,
+          adult: false,
+          title: 'J\'adore film'
+        }, {
+          budget: 10000000,
+          adult: false,
+          title: 'Deadpool'
+        }
+      ]
+    }, {
+      name: 'Liste 2',
+      movies: [
+        {
+          budget: 3000000,
+          adult: true,
+          title: 'Jolie film 2 '
+        }
+      ]
+    }, {
+      name: 'Liste 3',
+      movies: [
+        {
+          budget: 2000000,
+          adult: true,
+          title: 'Jolie film 3'
+        }
+      ]
+    }
+  ];
   private firebaseLists: AngularFireList<MovieList[]>;
   movieSubject: Subject<MovieList[]> = new Subject();
 
@@ -30,13 +71,25 @@ export class FirebaseService {
 
   saveMoviesLists(userId: string) {
     console.log('saving list data', userId);
-    this.firebase.database.ref(`/users/${userId}/lists/`).set(this.moviesListList);
+    this.moviesListList.forEach(e => {
+      this.firebase.database.ref(`/users/${userId}/lists/${e.name}`).set(e.movies);
+    });
   }
 
   getMoviesLists(userId: string) {
+    this.saveMoviesLists(userId);
     this.firebase.database.ref(`/users/${userId}/lists/`)
       .on('value', (data: DataSnapshot) => {
-          this.moviesListList = data.val() ? data.val() : [];
+        const newMoviesListList = [];
+        data.forEach(e => {
+          console.log('test ', e, e.key, e.val());
+          newMoviesListList.push({
+            name: e.key,
+            movies: e.val(),
+          });
+        });
+          // this.moviesListList = data.val() ? data.val() : [];
+          if (newMoviesListList.length !== 0) { this.moviesListList = newMoviesListList; }
           console.log('getting list film', data.val(), this.moviesListList);
           this.emmitUserMoviesList();
         }
